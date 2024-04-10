6b686e0ae8abe249d30cb63c2cd598ec766e4e60
Lim Geun <shing100@naver.com>
2024-04-11 01:48:37 +0900

    [20240411] 차트 모바일 크기 체크

--- src/components/salaryLineChart.tsx
+++ src/components/salaryLineChart.tsx
@@ -8,26 +8,12 @@ import {
     XAxis,
     YAxis
 } from "recharts";
-import {useEffect, useState} from "react";
 
 
 const salaryLineChart = ({ graphData } : any) => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 768);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const margin = isMobile ? { top: 0, right: 0, bottom: 0, left: 5 } : { top: 20, right: 30, bottom: 20, left: 30 };
-
     return (
         <ResponsiveContainer width="100%" height={400}>
-            <LineChart data={graphData} margin={margin}>
+            <LineChart data={graphData} margin={{ top: 20, right: 5, bottom: 20, left: 5 }}>
                 <XAxis dataKey="date" tick={{ fontSize: 14 }} tickFormatter={(value) => `${value}`} />
                 <YAxis type="number" domain={[20000000, 70000000]} tickFormatter={(value) => `${Math.floor(value / 10000)}만원`} tick={{ fontSize: 14 }} />
                 <CartesianGrid strokeDasharray="3 3" />
